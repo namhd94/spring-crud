8fac4a59978f2b562e9f3f3e063d1f2632fc608f
namhd94 <namhd94@gmail.com>
2019-11-09 14:05:48 +0700

    Push web app Update user rest

--- src/main/java/crudwebapp/webapp/controller/UserController.java
+++ src/main/java/crudwebapp/webapp/controller/UserController.java
@@ -4,9 +4,18 @@
 package crudwebapp.webapp.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import crudwebapp.webapp.entity.UserEntity;
@@ -17,15 +26,37 @@ import crudwebapp.webapp.service.UserService;
  *
  */
 @RestController
+@RequestMapping("/rest/user")
 public class UserController {
   
   @Autowired
   private UserService userService;
   
-  @GetMapping("/")
-  public List<UserEntity> index() {
-    List<UserEntity> user = userService.getAllUsers();
-    
-    return user;
+  @GetMapping
+  public List<UserEntity> findAll() {
+    return userService.getAllUsers();
+  }
+  
+  @PostMapping
+  public void createUser(@Valid @RequestBody UserEntity user) {
+	  userService.saveUser(user);
+  }
+  
+  @GetMapping("/{id}")
+  public Optional<UserEntity> readUser(@PathVariable Long id) {
+	  return userService.findUserById(id);
+  }
+  
+  @PutMapping("/{id}")
+  public void updateUser(@PathVariable Long id, @Valid @RequestBody UserEntity updatedUser) {
+	  Optional<UserEntity> user = userService.findUserById(id);
+	  if (user.isPresent()) {
+		  userService.saveUser(updatedUser);
+	  }
+  }
+  
+  @DeleteMapping("/{id}")
+  public void deleteUser(@PathVariable Long id) {
+	  userService.deleteUser(id);
   }
 }
