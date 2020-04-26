1cfde86bfbbedb6bd376306be686cae91bceda8c
namhd94 <namhd94@gmail.com>
2020-04-26 15:36:30 +0700

    Apply ng ant template

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { InternalErrorComponent } from './error/internal-error.component';
+import { NotFoundComponent } from './error/not-found.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: '500', component: InternalErrorComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
